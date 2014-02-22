a40e787fc331420d9d037cbcfd5153d8ae8f28df
Maya Saietz <mayasaietz@gmail.com>
2014-02-22 18:59:43 +0100

    Connect four!

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define GAME_CONFIG
 
 #define LINES  41
-#define WIDTH  20
-#define HEIGHT 10
+#define WIDTH  15
+#define HEIGHT 8
 
 #endif
--- main.c
+++ main.c
@@ -2,53 +2,71 @@
 #include "game/display.h"
 #include "game/config.h"
 
-int x = 10;
-int y = 5;
+#define MIDDLE 7
+int x = MIDDLE;
 
-void move(int dx, int dy) {
-    x += dx; y += dy;
+void move(int dx) {
+    x += dx;
     if(x < 1) x = 1;
-    if(y < 1) y = 1;
     if(x >= WIDTH-1) x = WIDTH-2;
-    if(y >= HEIGHT-1) y = HEIGHT-2;
 }
 
 int main(void) {
     display_t disp;
     char c;
-    int i;
+    int i,j;
+    int lowest[WIDTH-2];
+    char player = 'x';
     heap_init();
 
     display_init(&disp, WIDTH, HEIGHT);
 
     // Put in the walls
-    for(i = 0; i < disp.width; i++) {
-        display_set(&disp, '-', i, 0);
+    for(i = 1; i < disp.width-1; i++) {
+        display_set(&disp, '-', i, 1);
         display_set(&disp, '-', i, disp.height - 1);
     }
-    for(i = 1; i < disp.height-1; i++) {
+    for(i = 0; i < disp.height; i++) {
         display_set(&disp, '|', 0, i);
         display_set(&disp, '|', disp.width - 1, i);
     }
+    for(i = 2; i <= disp.width-3; i = i + 2) {
+        for(j = 2; j < disp.height-1; j++) {
+            display_set(&disp, '|', i, j);
+        }
+    }
+
+    // Lowest is HEIGHT-2 everywhere
+    for(i = 0; i < WIDTH-2; i++) {
+        lowest[i] = HEIGHT-2;
+    }
 
     // Place player and show!
-    display_set(&disp, 'x', x, y);
+    display_set(&disp, player, x, 0);
     display_flip(&disp);
     while((c = getc_raw()) != 'q') {
-        display_set(&disp, ' ', x, y);
+        display_set(&disp, ' ', x, 0);
         if(c == 'a') {
-            move(-1,0);
+            move(-2);
         } else if(c == 'd') {
-            move(1,0);
-        } else if(c == 'w') {
-            move(0,-1);
-        } else if(c == 's') {
-            move(0,1);
+            move(2);
+        } else if(c == ' ') {
+            // place marker
+            i = (x-1) / 2;
+            display_set(&disp, player, x, lowest[i]);
+            lowest[i]--;
+            // switch player
+            if(player == 'x') player = 'o';
+            else player = 'x';
+            // reset player pos
+            display_set(&disp, ' ', x, 0);
+            x = MIDDLE;
         }
-        display_set(&disp, 'x', x, y);
+        display_set(&disp, player, x, 0);
         display_flip(&disp);
     }
 
     syscall_halt(); // replace with syscall_exit(0) to run as a process
     return 0;
 }
+
